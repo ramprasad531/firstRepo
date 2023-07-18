7c2073086b4d19943408df10bb7a49ea92fdb20c
ramprasad531 <itsramprasad15@gmail.com>
2023-07-18 21:11:59 +0530

    my reversestring function

--- first.py
+++ first.py
@@ -1,2 +1,13 @@
 print("1st code")
-Print("2nd change")
+print("2nd change")
+
+
+def rev_string(str_n):
+    str_v=""
+    for i in str_n:
+        print(i)
+        str_v = i + str_v
+    print(str_v)
+rev_string("abhi")    
+
+        
